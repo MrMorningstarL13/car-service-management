f1cd68f542435aa20218be6d43f5d6ae77522b01
Leo <Leonard.Ion@adaptarobotics.com>
2025-04-25 14:31:42 +0300

    reset function

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
 const express = require('express')
 require('dotenv').config()
+const { db } = require('./models') 
 
 const port = process.env.PORT
 const app = express()
 
 app.listen(port, () => {
     console.log(`http://localhost:${port}`)
+})
+
+app.get('/reset', async (req, res) => {
+    try {
+        db.sync({force: true});
+        res.status(200).json({message: "reset ok"})
+    } catch(err) {
+        res.status(500).warn("there was an error during reset")
+    }
 })
