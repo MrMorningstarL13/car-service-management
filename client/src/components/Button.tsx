ca3e6699d94fcc2d9fbba77199cd4bb6f82938fe
Leo <Leonard.Ion@adaptarobotics.com>
2025-04-25 14:16:23 +0300

    small fix

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -20,7 +20,7 @@ const Button: React.FC<ButtonProps> = ({ buttonName, route }) => {
 
     return (
         <>
-            <button className="min-w-4 min-h-3 bg-blue-500 text-white p-1"
+            <button className="min-w-4 min-h-3 bg-contrast-primary text-white p-1"
             onClick={handleOnClick}
             >
                 {buttonName || "Default Button"}
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -2,10 +2,9 @@ import Button from './Button'
 
 const Navbar = () => {
     return (
-        <div className="absolute top-0 inset-x-0 w-full min-h-5 bg-green-500 flex justify-start items-center align gap-8 text-black p-1">
+        <div className="absolute top-0 inset-x-0 w-full min-h-5 bg-primary flex justify-start items-center align gap-8 text-black p-1">
             
            <input type='text' placeholder='Search auto service...' className='ml-10 border-2 border-solid rounded-md'/>
-            <a href="https://http.cat" target='blank'>Cats!</a>
 
             <Button buttonName="Home" route='/home'/>
             <Button buttonName="" route='/'/>
