81af8d8085a81351fcdb06adaec84887139ec6c6
MrMorningstarL <ion.1alexandruleonard@yahoo.ro>
2025-05-02 17:09:38 +0300

    submit btn

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -1,6 +1,10 @@
 
 export default function SignUp() {
 
+    const handleSubmitClick = () => {
+        alert('ma-ta dreacu\'')
+    }
+
     return (
         <main className="min-h-screen bg-[#f8f9f4] flex items-center justify-center">
             <div className="bg-primary rounded-xl p-4">
@@ -14,6 +18,7 @@ export default function SignUp() {
                     <label className="text-black">Password</label>
                     <input type="text" placeholder="type email here" className="w-full pl-10 py-2 pr-3 rounded-md border border-[rgba(119,150,109,1)] focus:border-[rgba(98,109,88,1)] text-[rgba(84,67,67,1)] focus:outline-none"/>
                 </div>
+                <button onClick={handleSubmitClick}>Submit</button>
 
             </div>
         </main>
